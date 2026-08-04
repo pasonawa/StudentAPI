306bf388be8becca1f88fdf0ddf5bd6174e27916
Pasonawa <sonawanepooja201920@gmail.com>
2026-08-04 11:04:09 +0530

    Initial commit

--- obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106c232cae745a95ba9f2024acfc6ac45997b74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599a4287e505448971169e43ddabf8f1ed3b58a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
